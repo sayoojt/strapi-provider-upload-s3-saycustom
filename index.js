f043985b5b24d8983cc971f907ff9307b874ad2f
sayoojt <sayoojt@users.noreply.github.com>
2022-07-09 15:59:52 +0400

    Support for STrapi 4

--- index.js
+++ index.js
@@ -18,30 +18,37 @@ module.exports = {
         if (!config.ACL) config.ACL = config.isPublic ? 'public-read' : 'private';
         let baseURLProtocol = "http://";
         if (config.isBaseUrlHttps) baseURLProtocol = "https://";
+        
+        const upload =  (file, customParams = {}) => {
+            new Promise((resolve, reject) => {
+               var objectKey = `${file.hash}${file.ext}`;
+               if (config.baseFolder)objectKey=`${config.baseFolder}/${objectKey}`;
+   
+               s3.upload({
+                   ACL: config.ACL ? config.ACL : 'private',
+                   ...customParams,
+                   Key: objectKey,
+                   Body: Buffer.from(file.buffer, 'binary'),
+                   ContentType: file.mime,
+               }, (err, data) => {
+                   if (err)
+                       return reject(err);
+                   if (config.baseUrl) {
+                       file.url = `${baseURLProtocol}${config.baseUrl}/${objectKey}`;
+                   }
+                   else {
+                       file.url = data.Location
+                   }
+                   resolve();
+               });
+           });
+        };        
         return {
+            uploadStream(file, customParams = {}) {
+                return upload(file, customParams);
+            },
             upload(file, customParams = {}) {
-                return new Promise((resolve, reject) => {
-                    var objectKey = `${file.hash}${file.ext}`;
-                    if (config.baseFolder)objectKey=`${config.baseFolder}/${objectKey}`;
-
-                    s3.upload({
-                        ACL: config.ACL ? config.ACL : 'private',
-                        ...customParams,
-                        Key: objectKey,
-                        Body: Buffer.from(file.buffer, 'binary'),
-                        ContentType: file.mime,
-                    }, (err, data) => {
-                        if (err)
-                            return reject(err);
-                        if (config.baseUrl) {
-                            file.url = `${baseURLProtocol}${config.baseUrl}/${objectKey}`;
-                        }
-                        else {
-                            file.url = data.Location
-                        }
-                        resolve();
-                    });
-                });
+                return upload(file, customParams);
             },
             delete(file, customParams = {}) {
                 return new Promise((resolve, reject) => {
